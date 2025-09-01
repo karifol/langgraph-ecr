8fe8cde9333f16c7b04aa068b2569b31ada48be3
karifol <karigolhorinouchi@gmail.com>
2025-09-01 22:17:44 +0900

    d

--- app.py
+++ app.py
@@ -78,7 +78,10 @@ def handler(event, context):
             "history": state["history"]
         }),
         "headers": {
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
         }
     }
 
